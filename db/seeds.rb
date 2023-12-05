646f40bfd01121eab084ad3cf25f86926430461b
Carson Cole <carson.cole@gmail.com>
2023-12-05 16:34:18 -0600

    head file added

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@ unless Source.any?
   end
 end
 
-user = create(:user, email: 'test@example.com', password: 'password')
+user = User.find_by_email('test@example.com') || create(:user, email: 'test@example.com', password: 'password')
 
 [1..20].each do
-  create_list(:job, 20, user: user, description: Faker::Lorem.paragraph)
+  create_list(:job, 20, user: user, description: Faker::Lorem.paragraph, mode: 'office', arrangement: 'permanent')
 end
 
 Job.all.each do |j|
