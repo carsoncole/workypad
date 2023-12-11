0192fc3ad8fe61cbe7cae4678919dc9e94684f94
Carson Cole <carson.cole@gmail.com>
2023-12-11 11:24:54 -0800

    test: Add contact, salary data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,12 @@ user = User.find_by_email('test@example.com') || create(:user, email: 'test@exam
     arrangement: Job.arrangements.pluck(0)[rand(Job.arrangements.size - 1) + 1],
     source: Source.find(rand(Source.count) + 1),
     status: Job.statuses.pluck(0)[rand(Job.statuses.size)],
-    url: Faker::Internet.url
-    )
+    url: Faker::Internet.url,
+    primary_contact_name: Faker::Name.name,
+    primary_contact_phone: Faker::PhoneNumber.phone_number,
+    primary_contact_email: Faker::Internet.email,
+    salary: "$#{rand(200) + 75},000"
+  )
 end
 
 Job.all.each do |j|
