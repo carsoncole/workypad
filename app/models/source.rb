b91684c1d6352e293a68ce1b348b5147021b3d3d
Carson Cole <carson.cole@gmail.com>
2023-12-15 16:17:53 -0800

    feat: add Simply Hired as a Source

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,3 +1,3 @@
 class Source < ApplicationRecord
-  SOURCES = ["Indeed", "Other", "Ziprecruitor", "LinkedIn", "Dice", "Google", "Bing", "Referral", "Jobot", "Company site"]
+  SOURCES = ["Indeed", "Other", "Ziprecruitor", "LinkedIn", "Dice", "Google", "Bing", "Referral", "Jobot", "Simply Hired", "Company site"]
 end
